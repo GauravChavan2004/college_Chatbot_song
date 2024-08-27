58a7c57ec81b51133429214fb63280c56a87b17e
GauravChavan2004 <gauravchavan101004@gmail.com>
2024-08-27 09:43:32 +0530

    add navbar and logo

--- ChatBot_Song/settings.py
+++ ChatBot_Song/settings.py
@@ -126,3 +126,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR , "static",
 ]
+
+MEDIA_URL='/media/'
+MEDIA_ROOT = BASE_DIR /'media'
--- ChatBot_Song/urls.py
+++ ChatBot_Song/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from chatbot_app import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,7 @@ urlpatterns = [
     path("chat/", views.chat),
 
 ]
+
+if settings.DEBUG: 
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
